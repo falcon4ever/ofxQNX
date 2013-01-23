7a58581b65154fec675c935cedd0f30c4ac02a4e
Laurence Muller <laurence.muller@gmail.com>
2013-01-23 21:35:08 +0100

    Added ogg library

---
+++ bootstrap/ogg-bb10.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+
+# Get NDK_ROOT
+source paths.make
+
+# Bootstrap folder
+BB_BOOTSTRAP_ROOT=$(pwd)
+BB_BOOTSTRAP_ROOT=${BB_BOOTSTRAP_ROOT%/*}
+
+# Build BB10 platform
+if [ -d "$BBNDK_BB10_ROOT" ]; then
+
+	# Get build environment	
+	source "$BBNDK_BB10_ROOT/bbndk-env.sh"	
+	cd "$BB_BOOTSTRAP_ROOT/bootstrap/ogg"	
+	
+	# Build libs
+	${QNX_HOST}/usr/bin/make clean
+	${QNX_HOST}/usr/bin/make
+
+	mkdir -p "$BB_BOOTSTRAP_ROOT/libs/ogg/include/ogg"
+	mkdir -p "$BB_BOOTSTRAP_ROOT/libs/ogg/libBB10/X86/Release"
+	mkdir -p "$BB_BOOTSTRAP_ROOT/libs/ogg/libBB10/X86/Debug"
+	mkdir -p "$BB_BOOTSTRAP_ROOT/libs/ogg/libBB10/ARMv7/Release"
+	mkdir -p "$BB_BOOTSTRAP_ROOT/libs/ogg/libBB10/ARMv7/Debug"
+
+	cp ogg/ogg.h "$BB_BOOTSTRAP_ROOT/libs/ogg/include/ogg"
+	cp ogg/os_types.h "$BB_BOOTSTRAP_ROOT/libs/ogg/include/ogg"
+	cp x86/a/libogg.a "$BB_BOOTSTRAP_ROOT/libs/ogg/libBB10/X86/Release"
+	cp x86/a-g/libogg.a "$BB_BOOTSTRAP_ROOT/libs/ogg/libBB10/X86/Debug"
+	cp arm/a.le-v7/libogg.a "$BB_BOOTSTRAP_ROOT/libs/ogg/libBB10/ARMv7/Release"
+	cp arm/a.le-v7-g/libogg.a "$BB_BOOTSTRAP_ROOT/libs/ogg/libBB10/ARMv7/Debug"	
+fi
